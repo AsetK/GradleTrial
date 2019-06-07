12b3f9dffbba255ec0608c6ccc4c33ea8c0b0199
Asset_Kenezhanov <bysskc@mail.ru>
2019-06-07 17:15:16 +0600

    tasks: 1-commit

--- src/main/java/com/epam/helloworldgradle/Runner.java
+++ src/main/java/com/epam/helloworldgradle/Runner.java
@@ -1,6 +1,7 @@
 package com.epam.helloworldgradle;
 
 public class Runner {
+
     public static void main(String[] args) {
         System.out.println("hello gradle");
     }
--- src/test/java/com/epam/calculator/IntegrationalTest.java
+++
@@ -1,7 +0,0 @@
-package com.epam.calculator;
-
-public class IntegrationalTest {
-    public static void main(String[] args) {
-        System.out.println("Integrational Test");
-    }
-}
