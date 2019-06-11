29d79625637def6033289902c8acf424028ea8bd
Asset_Kenezhanov <bysskc@mail.ru>
2019-06-11 17:44:01 +0600

    tasks: 5-commit: Test coverage. If test fails,jar command fails too. To package: gradle jar .

---
+++ src/test/java/com/epam/helloworldgradle/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.epam.helloworldgradle.calculator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void add() {
+        int expResult = 5;
+        int result = Calculator.add(2,4);
+        assertEquals(expResult, result);
+    }
+}
